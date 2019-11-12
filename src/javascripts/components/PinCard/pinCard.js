a3720a729ed5753b2cc9b1b5f70f2a1b69f014c6
Ivan Phelps <ivanniothemannio@gmail.com>
2019-11-11 19:56:38 -0600

    delete pin works

--- src/javascripts/components/PinCard/pinCard.js
+++ src/javascripts/components/PinCard/pinCard.js
@@ -11,8 +11,8 @@ const makeAPin = (pins) => {
       <div class="card-body">
         <h5 class="card-title">${pins.name}</h5>
         <p class="card-text">${pins.description}</p>
-        <a href="${pins.url}" target="_blank"Ks><button type="button" class="btn btn-outline-success">Visit Site</button></a>
-        <button type="button" class="btn btn-outline-danger">Delete Pin</button>
+        <a href="${pins.url}" target="_blank"><button type="button" class="btn btn-outline-success">Visit Site</button></a>
+        <button type="button" class="btn btn-outline-danger delete-pin" id="delete-${pins.id}">Delete Pin</button>
       </div>
     </div>
   </div>
